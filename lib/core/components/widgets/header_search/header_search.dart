5cab75091349eb5f503d506d05545848608d7f4b
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-08-01 23:25:40 +0700

    🐼 Add some type custom for header search

--- lib/core/components/widgets/header_search/header_search.dart
+++ lib/core/components/widgets/header_search/header_search.dart
@@ -3,11 +3,22 @@ import 'package:flutter_base_clean_architecture/core/components/extensions/strin
 
 import 'model_bottom_sheet.dart';
 
+const _kRadius = 14.0;
+
 class HeaderSearch extends StatefulWidget {
   final List<String> colors;
   final String hintText;
+  final TextStyle? hintStyle;
+  final TextStyle? textStyle;
   final double vPadding;
   final double hPadding;
+  final double? searchRadius;
+  final Widget? prefixIcon;
+  final Function(String)? textChange;
+  final bool isShowAction;
+  final Widget? actionIcon;
+  final Function()? onPress;
+  final Function(dynamic)? filterCall;
 
   const HeaderSearch({
     super.key,
@@ -15,6 +26,15 @@ class HeaderSearch extends StatefulWidget {
     this.hintText = 'Search',
     this.vPadding = 10.0,
     this.hPadding = 20.0,
+    this.hintStyle,
+    this.textStyle,
+    this.textChange,
+    this.prefixIcon,
+    this.searchRadius,
+    this.isShowAction = true,
+    this.actionIcon,
+    this.onPress,
+    this.filterCall,
   });
 
   @override
@@ -22,7 +42,7 @@ class HeaderSearch extends StatefulWidget {
 }
 
 class _HeaderSearchState extends State<HeaderSearch> {
-  List<Color> listStringtoColors(List<String> colors) {
+  List<Color> listStringToColors(List<String> colors) {
     List<Color> result = [];
     for (var i = 0; i < colors.length; i++) {
       result.add(colors[i].toColor());
@@ -30,16 +50,44 @@ class _HeaderSearchState extends State<HeaderSearch> {
     return result;
   }
 
+  void _filterOnTap() async {
+    final data = await showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(14),
+          topRight: Radius.circular(14),
+        ),
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      builder: (context) {
+        return const ModelBottomSheet();
+      },
+    );
+    if (data != null && widget.filterCall != null) {
+      widget.filterCall?.call(data);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final textColor = Theme.of(context).scaffoldBackgroundColor;
+    final hintStyle = widget.hintStyle ?? TextStyle(color: textColor);
+    final textStyle = widget.textStyle ??
+        TextStyle(fontWeight: FontWeight.w800, fontSize: 16, color: textColor);
+    final prefixIcon =
+        widget.prefixIcon ?? Icon(Icons.search, color: textColor);
+
+    final searchRadius = widget.searchRadius ?? _kRadius;
+    final actionIcon = widget.actionIcon ??
+        Icon(Icons.filter_list_outlined, size: 30, color: textColor);
 
     return Column(
       children: [
         Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: listStringtoColors(widget.colors),
+              colors: listStringToColors(widget.colors),
               begin: Alignment.centerLeft,
               end: Alignment.centerRight,
             ),
@@ -47,54 +95,35 @@ class _HeaderSearchState extends State<HeaderSearch> {
           child: SafeArea(
             child: Container(
               padding: EdgeInsets.symmetric(
-                  horizontal: widget.hPadding, vertical: widget.vPadding),
+                horizontal: widget.hPadding,
+                vertical: widget.vPadding,
+              ),
               child: Row(
                 children: [
                   Expanded(
                     child: TextField(
                       cursorColor: textColor,
-                      style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                          color: textColor),
+                      onChanged: widget.textChange,
+                      style: textStyle,
                       decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: textColor,
-                          ),
-                          fillColor: textColor.withOpacity(0.25),
-                          filled: true,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(14),
-                              borderSide: BorderSide.none),
-                          hintText: widget.hintText,
-                          hintStyle: TextStyle(color: textColor)),
+                        prefixIcon: prefixIcon,
+                        fillColor: textColor.withOpacity(0.25),
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(searchRadius),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: widget.hintText,
+                        hintStyle: hintStyle,
+                      ),
                     ),
                   ),
                   const SizedBox(width: 10),
-                  GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet(
-                        context: context,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(14),
-                            topRight: Radius.circular(14),
-                          ),
-                        ),
-                        backgroundColor:
-                            Theme.of(context).scaffoldBackgroundColor,
-                        builder: (context) {
-                          return const ModelBottomSheet();
-                        },
-                      );
-                    },
-                    child: Icon(
-                      Icons.filter_list_outlined,
-                      size: 30,
-                      color: textColor,
+                  if (widget.isShowAction)
+                    GestureDetector(
+                      onTap: _filterOnTap,
+                      child: actionIcon,
                     ),
-                  ),
                 ],
               ),
             ),
